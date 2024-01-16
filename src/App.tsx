d9e01a82ea1b022fe93c1bc775a4b420ea4f27ee
johnyworld <johnyworld@naver.com>
2024-01-16 15:20:07 +0900

    feat: 리모트 유저 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import AgoraRTC, { IAgoraRTCRemoteUser } from 'agora-rtc-sdk-ng';
+import AgoraRTC, { IAgoraRTCRemoteUser, IDataChannelConfig } from 'agora-rtc-sdk-ng';
 import { useEffect, useMemo, useState } from 'react';
 import { LocalUserVideo } from './components/LocalUserVideo';
 import { useLocalTracks } from './hooks/useLocalTracks';
+import { RemoteUserVideo } from './components/RemoteUserVideo';
+
+type AgoraClientEventListener = (
+  user: IAgoraRTCRemoteUser,
+  mediaType: 'audio' | 'video' | 'datachannel',
+  config?: IDataChannelConfig | undefined
+) => void;
 
 const APP_ID = import.meta.env.VITE_AGORA_APP_ID;
 const roomId = 'main';
@@ -16,27 +23,58 @@ if (!uid) {
 function App() {
   const [localAudioTrack, localVideoTrack] = useLocalTracks();
 
+  const [remoteUsers, setRemoteUsers] = useState<IAgoraRTCRemoteUser[]>([]);
+
+  const [isJoined, setIsJoined] = useState(false);
+
   const client = useMemo(() => AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' }), []);
 
   useEffect(() => {
-    if (!client) {
+    const join = async () => {
+      await client.join(APP_ID, roomId, token, uid);
+      setIsJoined(true);
+    };
+    join();
+  }, [client]);
+
+  useEffect(() => {
+    const handleUserPublished: AgoraClientEventListener = async (user, mediaType) => {
+      console.log('=== USER PUBLISHED');
+      await client.subscribe(user, mediaType);
+      setRemoteUsers([...remoteUsers, user]);
+    };
+    const handleUserUnpublished: AgoraClientEventListener = async (user, mediaType) => {
+      console.log('=== USER UN-PUBLISHED');
+      await client.unsubscribe(user, mediaType);
+      setRemoteUsers(remoteUsers.filter(remoteUser => remoteUser.uid !== user.uid));
+    };
+    client.on('user-published', handleUserPublished);
+    client.on('user-unpublished', handleUserUnpublished);
+    return () => {
+      client.off('user-published', handleUserPublished);
+      client.off('user-unpublished', handleUserUnpublished);
+    };
+  }, [client, remoteUsers]);
+
+  useEffect(() => {
+    if (!isJoined) {
       return;
     }
     const main = async () => {
-      await client.join(APP_ID, roomId, token, uid);
-
-      if (localVideoTrack && localVideoTrack) {
+      if (localAudioTrack && localVideoTrack) {
         client.publish([localAudioTrack, localVideoTrack]);
       }
     };
-
     main();
-  }, [client, localAudioTrack, localVideoTrack]);
+  }, [client, isJoined, localAudioTrack, localVideoTrack, remoteUsers]);
 
   return (
     <main>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       {localVideoTrack && <LocalUserVideo localVideoTrack={localVideoTrack} />}
+      {remoteUsers.map(user => (
+        <RemoteUserVideo key={user.uid} user={user} />
+      ))}
     </main>
   );
 }
---
+++ src/components/RemoteUserVideo.tsx
@@ -0,0 +1,18 @@
+import { IAgoraRTCRemoteUser } from 'agora-rtc-sdk-ng';
+import { useEffect, useRef } from 'react';
+
+interface Props {
+  user: IAgoraRTCRemoteUser;
+}
+
+export const RemoteUserVideo = ({ user }: Props) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (divRef.current) {
+      user.videoTrack?.play(divRef.current);
+    }
+  }, [user.videoTrack]);
+
+  return <div ref={divRef} className='h-20 w-32' />;
+};
